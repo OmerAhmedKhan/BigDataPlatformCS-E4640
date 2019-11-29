a2d2ea1c04c991c2a38e49c151e492c7ff34121e
oak <omerahmed122@gmail.com>
2019-11-29 03:37:06 +0200

    Deployment script and self grading

---
+++ assignment-3-802062/code_assignment_2/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+wget -O- https://dl.bintray.com/rabbitmq/Keys/rabbitmq-release-signing-key.asc | sudo apt-key add -
+wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
+echo "deb https://dl.bintray.com/rabbitmq/debian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/rabbitmq.list
+sudo apt update
+sudo apt -y install rabbitmq-server
+sudo rabbitmq-plugins enable rabbitmq_management
